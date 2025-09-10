b9258f3f78242c5338e335a8bc0650f6f68caa4b
ahmadsorann <ahmadsorann@gmail.com>
2025-09-10 16:26:23 +0300

    Remove obsolete Workbox core file and its associated code

--- next.config.js
+++ next.config.js
@@ -4,75 +4,6 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   sw: 'sw.js',
-  fallbacks: {
-    // Don't redirect to offline page, let the app handle offline state
-    document: null,
-    image: null,
-    audio: null,
-    video: null,
-    font: null,
-  },
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts',
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts-static',
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:js|css|woff|woff2|ttf|eot)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-assets',
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      // Cache all navigation requests (pages)
-      urlPattern: /^https?:\/\/[^\/]+\/(?!api\/).*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'pages-cache',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
 })
 
 /** @type {import('next').NextConfig} */
@@ -80,10 +11,6 @@ const nextConfig = {
   eslint: {
     // Disable ESLint during builds
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Disable type checking during builds (optional)
-    ignoreBuildErrors: false,
   },
 }
 
